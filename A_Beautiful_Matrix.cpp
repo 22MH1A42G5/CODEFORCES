c23382fa61d83373d1414ed34664e9234dee24c5
KAMPARAPU SRI RAM <kamparapusriram@gmail.com>
2024-12-29 22:50:23 +0530

    Add files via upload

---
+++ A_Beautiful_Matrix.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define vi vector<ll>
+#define ll long long
+#define ld long double
+#define pb push_back
+#define f first
+#define s second
+#define ALL(x) begin(x), end(x)
+#define fur(i, n) for (int i = 0; i < n; i++) 
+#define rev(x) reverse(x.begin(), x.end());
+#define sort(x) sort(x.begin(), x.end())
+#define rsort(x) sort(x.rbegin(), x.rend())
+#define MAX(x) *max_element(ALL(x))
+#define MIN(x) *min_element(ALL(x))
+using namespace std;
+int main(){
+    int d,t1,t2;
+    int i ;
+    for(i = 0 ; i < 25 ; i++){
+        cin>>d;
+        if(d == 1){
+            break;
+        }
+    }
+    cout<<abs(i/5-2)+abs(i%5-2);
+}
