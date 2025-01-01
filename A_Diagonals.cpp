14191ee0b91d54b12a977505f6aa64d2e52970db
KAMPARAPU SRI RAM <kamparapusriram@gmail.com>
2025-01-01 19:29:44 +0530

    Add files via upload

--- A_Diagonals.cpp
+++ A_Diagonals.cpp
@@ -1,29 +1,43 @@
 #include<bits/stdc++.h>
+#define vi vector<ll>
+#define ll long long
+#define ld long double
+#define pb push_back
+#define f first
+#define s second
+#define ALL(x) begin(x), end(x)
+#define fur(i, n) for (int i = 0; i < n; i++) 
+#define rev(x) reverse(x.begin(), x.end());
+#define sort(x) sort(x.begin(), x.end())
+#define rsort(x) sort(x.rbegin(), x.rend())
+#define MAX(x) *max_element(ALL(x))
+#define MIN(x) *min_element(ALL(x))
 using namespace std;
-int main(){
-  int t;
-  cin>>t;
-  while(t--){
-    int n,k,cnt=1;
-    cin>>n>>k;
-    if(k==0){
-        cout<<0<<endl;
-        continue;
-    }
-    if(k<=n){
-        cout<<1<<endl;
-        continue;
-    }
+int Diagonals(int n , int k){
+    if(k == 0)
+    return 0;
+    if(k <= n)
+    return 1;
+    int i = n-1;
+    int cnt = 0,ans = 0;
     k-=n;
-    while(n>1 && k>0){
-        k-=n-1;
+    while( k > 0){
+        k -= i;
         cnt++;
-        if(k<=0)
-            break;
-        k-=n-1;
-        cnt++;
-        n-=1;
+        if(cnt == 2){
+            cnt = 0;
+            i--;
+        }
+        ans++;
     }
-    cout<<cnt<<endl;
-  }
+    return ans+1;
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        cout<<Diagonals(n , k)<<"\n";
+    }
 }
