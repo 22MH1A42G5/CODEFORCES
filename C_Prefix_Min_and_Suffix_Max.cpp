2a5df4068d7f15ed11a539fe169b4f647aa131df
ram <kamparapusriram@gmail.com>
2025-08-07 19:01:51 +0530

    B and C of contest

--- C_Prefix_Min_and_Suffix_Max.cpp
+++ C_Prefix_Min_and_Suffix_Max.cpp
@@ -12,31 +12,35 @@
 #define rsort(x) sort(x.rbegin(), x.rend())
 #define MAX(x) *max_element(ALL(x))
 #define MIN(x) *min_element(ALL(x))
-#define COUNT(x , t) count(ALL(x), t);
 using namespace std;
 int main(){
     ios_base::sync_with_stdio(false);cin.tie(NULL);
     ll t;
     cin >> t;
     while(t--){
-        ll n , j , k;
-        cin >> n >> j >> k;
+        ll n;
+        cin >> n;
         vi vec(n);
         fur(i , n) cin >> vec[i];
-        ll maxi = MAX(vec);
-        ll cnt = COUNT(vec , maxi);
-        j = vec[j-1];
-        if(j != maxi){
-            if(k != 1){
-                cout << "YES";
+        vi pref(n , 0) , suf(n , 0);
+        pref[0] = vec[0];
+        suf[n-1] = vec[n-1];
+        for(int i = 1 ; i < n ; i++){
+            pref[i] = min(pref[i-1] , vec[i]);
+        }
+        for(int i = n-2 ; i >= 0 ; i--){
+            suf[i] = max(suf[i+1],vec[i]);
+        }
+        string ans = "";
+        fur(i , n){
+            if(pref[i] == vec[i] || suf[i] == vec[i]){
+                ans = ans + "1";
             }
             else{
-                cout << "NO";
+                ans = ans + "0";
             }
         }
-        else{
-            cout << "YES";
-        }
-        cout << "\n";
+        cout << ans << "\n";
+        
     }
 }
